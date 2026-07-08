df439f67987eb5fb90dc8cf81297a7572eedca80
Valerii Babaiev <valerii_babaiev@epam.com>
2026-07-08 20:07:16 +0300

    feat: Register Comment model in admin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -7,3 +7,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('is_published',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at', 'content')
+    list_filter = ('created_at', 'post')
+    search_fields = ('name', 'content', 'post__title')
