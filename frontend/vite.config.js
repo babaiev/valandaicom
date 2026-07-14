6363f499ab4ee4ebe447cd16978c1b7e285034c4
Valerii Babaiev <valerii_babaiev@epam.com>
2026-07-14 23:00:59 +0300

    Tweak background colors to be lighter and add vite proxy for local API development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -44,4 +44,12 @@ export default defineConfig({
       all: true
     }
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
